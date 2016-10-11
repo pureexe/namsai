b73e5ad14e5b152a45ebfe9508fc69645883a5bb
Pakkapon Phongthawee <me@pureapp.in.th>
2016-10-11 21:11:31 +0700

    update: login and signup

--- routes/v1/users/register.php
+++ routes/v1/users/register.php
@@ -16,54 +16,48 @@ $app->post('/',function() use ($app,$config,$pdo){
   $password = $app->request->post("password");
   $username = $app->request->post("username");
   $name = $app->request->post("name");
-  if(!$email||!$password||!$username||!$name){
+  if(!isset($name)){
+    $name = "";
+  }
+  if(!isset($email)||!isset($password)||!isset($username)){
     $app->render(400,array(
       'error'=>array(
         'code'=>4,
         'message'=>'parameter name,email,username and password is require for register',
       ),
     ));
-  }else{
-    $passwordHasher = new Pentagonal\Phpass\PasswordHash(8,false);
-    $query = $pdo->select()->from('user')->where('user_email','=',$email);
-    $count = $query->execute()->rowCount();
-    if($count!=0){
-      $app->render(400,array(
-        'error'=>array(
-          'code'=>5,
-          'message'=>'this email has been already register',
-        ),
-      ));
-    }else{
-      $query = $pdo->select()->from('user')->where('user_name','=',$username);
-      $count = $query->execute()->rowCount();
-      if($count!=0){
-        $app->render(400,array(
-          'error'=>array(
-            'code'=>6,
-            'message'=>'this username has been already register',
-          ),
-        ));
-      }else{
-        if(($pos = array_search($username, $config['reserved_username'])) !== false){
-          $app->render(400,array(
-            'error'=>array(
-              'code'=>7,
-              'message'=>'username '.$username.' is reserved for system',
-            ),
-          ));
-        }else{
-          $pdo
-            ->insert(array('user_name','user_email','user_fullname','user_hash'))
-            ->into('user')
-            ->values(array($username,$email,$name,$passwordHasher->hashPassword($password)))
-            ->execute();
-          $app->render(200,array(
-            'id'=>intval($pdo->lastInsertId())
-          ));
-        }
-      }
-    }
+    return;
   }
+  if(User::isUserNameExist($username)){
+    $app->render(400,array(
+      'error'=>array(
+        'code'=>6,
+        'message'=>'this username has been already register',
+        ),
+    ));
+    return;
+  }
+  if(User::isEmailExist($email)){
+    $app->render(400,array(
+      'error'=>array(
+        'code'=>5,
+        'message'=>'this email has been already register',
+      ),
+    ));
+    return;
+  }
+  if(User::isReserved($username)){
+    $app->render(400,array(
+      'error'=>array(
+        'code'=>7,
+        'message'=>'username '.$username.' is reserved for system',
+      ),
+    ));
+    return;
+  }
+  $id = User::add($username,$email,$password,$name);
+  $app->render(200,array(
+    'id'=>intval($id)
+  ));
 });
 ?>
